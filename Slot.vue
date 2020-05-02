b24499186608b329bb1ff5eb91a636717e2eab04
masanarih0ri <masanarihoridevelop@gmail.com>
2020-05-02 22:50:40 +0900

    add slot

---
+++ Slot.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Layout>
+      <!-- slot属性の名前がheaderとすると、Layoutコンポーネントにあるslotタグのname属性がheaderの箇所に、「Header」というテキストがセットされている -->
+      <template slot="header">Header</template>
+      Main Contents
+      <span slot="footer">footer</span>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from "./components/Layout.vue";
+
+export default {
+  components: {
+    Layout
+  }
+};
+</script>
