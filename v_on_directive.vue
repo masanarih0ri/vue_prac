2fbee286ca08364894292c17c12551ab29429ffe
masanarih0ri <masanarihoridevelop@gmail.com>
2020-05-02 15:17:56 +0900

    add v_on_directive

---
+++ v_on_directive.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="clickLog">クリック</button>
+    <button @mouseenter="hoverLog">hover</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello"
+    };
+  },
+  methods: {
+    clickLog(event) {
+      console.log(event);
+      console.log(event.target);
+      console.log(this.message);
+    },
+    hoverLog(event) {
+      console.log(event);
+      console.log(event.target);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
