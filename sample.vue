1831d53ad8f43afaf5b10005ecd601bcdbaae12b
masanarih0ri <masanarihoridevelop@gmail.com>
2020-05-02 14:46:40 +0900

    add computed_sample

--- sample.vue
+++ sample.vue
@@ -1,13 +1,47 @@
 <template>
-  <p>{{ message }}</p>
+  <div :title="message">
+    {{ message }}
+    <p>{{ sum | numberWithDelimiter }}円</p>
+    <button :disabled="isButtonDisadled" @click="log">{{ buttonLabel }}</button>
+    <button :disabled="isButtonDisadled">{{ buttonLabel }}</button>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello, world!"
+      message: "sample",
+      sum: undefined,
+      isButtonDisadled: false
     };
+  },
+  filters: {
+    // valueを受け取って処理をする
+    numberWithDelimiter(value) {
+      if (!value) {
+        return "0";
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+    }
+  },
+  computed: {
+    // メソッドはデータが変更された時のみ呼び出されて、それ以外の場合はキャッシュが返される
+    buttonLabel() {
+      console.log("isButtonDisadled, called");
+      return this.isButtonDisadled ? "無効" : "有効";
+    },
+    // 関連するデータが変更されない限り、一度実行して、キャッシュされた値を返すので、再実行されない
+    // あくまでcomputedプロパティでは、データとして処理を加えた結果を値として提供したい場合に使う
+    // このnowメソッドは常に同じ時刻を返し続ける
+    now() {
+      return new Date().toTimeString();
+    }
+  },
+  methods: {
+    log() {
+      console.log(this.now);
+    }
   }
 };
 </script>
